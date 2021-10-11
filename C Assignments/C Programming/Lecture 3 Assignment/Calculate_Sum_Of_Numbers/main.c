087ad602421c541713c5718c43a979a620a51375
Youssef Maaty <youssefmohamed2374@gmail.com>
2021-10-11 14:58:07 +0200

    Add Parent Folder for Assignments

---
+++ C Assignments/C Programming/Lecture 3 Assignment/Calculate_Sum_Of_Numbers/main.c
@@ -0,0 +1,21 @@
+///*
+// * main.c
+// *
+// *  Created on: Sep 26, 2021
+// *      Author: youss
+// */
+//#include <stdio.h>
+//int main()
+//{
+//	int number ,sum=0 ,i ;
+//	printf("Enter an integer :");
+//	fflush(stdout);
+//	scanf("%d",&number);
+//	for(i=1;i<=number;i++)
+//	{
+//		sum +=i;
+//	}
+//	printf("Sum = %d",sum);
+//	return 0;
+//}
+//
---
+++ C Assignments/C Programming/Lecture 3 Assignment/Find_factorial/main.c
@@ -0,0 +1,30 @@
+///*
+// * main.c
+// *
+// *  Created on: Sep 26, 2021
+// *      Author: youss
+// */
+//#include <stdio.h>
+//int main()
+//{
+//	int number;
+//	printf("Enter an integer :");
+//	fflush(stdout);
+//	scanf("%d",&number);
+//
+//	if(number < 0) printf("Error!!! Factorial of negative number does not exist.");
+//
+//	else if(number == 0) printf("Factorial = 1");
+//
+//	else
+//	{
+//		int sum = 1;
+//		for(int i = 1 ; i<=number ; i++)
+//		{
+//			sum *=i;
+//		}
+//		printf("Factorial = %d",sum);
+//	}
+//	return 0;
+//}
+//

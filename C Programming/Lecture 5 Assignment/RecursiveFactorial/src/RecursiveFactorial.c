468914a9d4f2ef49f598b5e87c729e979359b2c2
Youssef Maaty <youssefmohamed2374@gmail.com>
2021-10-14 16:10:00 +0200

    Submit Assignment 5

---
+++ C Programming/Lecture 5 Assignment/RecursiveFactorial/src/RecursiveFactorial.c
@@ -0,0 +1,31 @@
+///*
+// ============================================================================
+// Name        : RecursiveFactorial.c
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+// ============================================================================
+// */
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//unsigned int Recursive_Factorial(unsigned int num);
+//unsigned int Recursive_Factorial(unsigned int num)
+//{
+//	//base condition
+//	if(num == 1 || num == 0 ) return 1 ;
+//	//recursive condition
+//	return num * Recursive_Factorial(num-1);
+//}
+//
+//
+//int main(void) {
+//	unsigned int num;
+//	printf("Enter a positive integer : ");
+//	fflush(stdout);
+//	scanf("%d",&num);
+//	printf("Factorial of %d = %d",num,Recursive_Factorial(num));
+//
+//}
